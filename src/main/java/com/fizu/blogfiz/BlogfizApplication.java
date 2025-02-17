1801a1ede5c85ef2c793f9ee6d7bfa52ddb33302
unknown <afisfisabilillah21@gmail.com>
2025-02-17 23:48:12 +0700

    starter project

---
+++ src/main/java/com/fizu/blogfiz/BlogfizApplication.java
@@ -0,0 +1,13 @@
+package com.fizu.blogfiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogfizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogfizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizu/blogfiz/BlogfizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizu.blogfiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogfizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
